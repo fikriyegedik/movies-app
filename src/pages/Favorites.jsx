17363aade43752681fc0dd64569a92ccffeb346a
Fikriye Gedikoğlu <fikriye.gedikoglu@yorkandchapel.com>
2025-05-02 16:21:34 +0300

    .

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,6 +1,6 @@
     import "../css/Favorites.css";
     import { useMovieContext } from "../contexts/MovieContext";
-    import MovieCard from "../components/MovieCard";
+    import MovieCard from "../Components/MovieCard";
 
     function Favorites() {
     const { favorites } = useMovieContext();
